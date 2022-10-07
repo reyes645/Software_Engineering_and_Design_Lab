4f183055005c9711bf3b920f6bc1cfc6e4e6eb6d
kevinp0122 <61161879+kevinp0122@users.noreply.github.com>
2022-10-07 03:33:44 -0500

    updated to correct code, implemented check in/out

--- hardware.py
+++ hardware.py
@@ -1,3 +1,7 @@
+import driver
+from database import Database as db
+
+
 class Hardware:
     capacity1 = 0
     capacity2 = 0
@@ -11,16 +15,42 @@ class Hardware:
         Hardware.availability2 = av2
 
     def check_out_hw(self, one, two):
-<<<<<<< HEAD
-        #implement pls
-        pass
+        # implement pls
+        cursor = db.hardware_collection.find({'hw1': Hardware.capacity1})
+        for temp in cursor:
+            avail1 = temp['availHW1']
+        for temp in cursor:
+            avail2 = temp['availHW2']
+
+        # check to see if you can actually check out this much from hw1 and hw2
+        # maybe have a return statement for failure
+        if one > avail1:
+            # do something
+            pass
+        if two > avail2:
+            # do something
+            pass
+
+        avail1 = avail1 - one
+        avail2 = avail2 - two
+
+        db.hardware_collection.update_one({'hw1': Hardware.capacity1}, {"$set": {'availHW1': avail1}})
+        db.hardware_collection.update_one({'hw1': Hardware.capacity1}, {"$set": {'availHW2': avail2}})
+
+        # maybe have a statement saying success
 
     def check_in_hw(self, one, two):
-        #implement pls
-=======
+        # implement pls
+        cursor = db.hardware_collection.find({'hw1': Hardware.capacity1})
+        for temp in cursor:
+            avail1 = temp['availHW1']
+        for temp in cursor:
+            avail2 = temp['availHW2']
 
-        pass
+        avail1 = avail1 + one
+        avail2 = avail2 + two
 
-    def check_in_hw(self, one, two):
->>>>>>> 718e7b04766f0b1112aad1688bedc7369680b391
-        pass
+        db.hardware_collection.update_one({'hw1': Hardware.capacity1}, {"$set": {'availHW1': avail1}})
+        db.hardware_collection.update_one({'hw1': Hardware.capacity1}, {"$set": {'availHW2': avail2}})
+
+        # maybe have statement saying success
