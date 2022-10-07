86b98500eada4510d3bfd8aea52597f4aab8ffd7
kevinp0122 <61161879+kevinp0122@users.noreply.github.com>
2022-10-07 03:36:23 -0500

    updated to correct code, project_page returns user projs

--- user.py
+++ user.py
@@ -1,5 +1,6 @@
 import encryption
 from project import Project
+from database import Database as db
 
 class User:
     username = ''
@@ -12,11 +13,7 @@ class User:
     def __init__(self):
         pass
 
-<<<<<<< HEAD
     def signup(self, usern, passw, db):
-=======
-    def signup(self, usern, passw):
->>>>>>> 718e7b04766f0b1112aad1688bedc7369680b391
         #Check if usern or passw contains ' ' or !, if yes, return error, if not continue and return
         if ' ' in usern or '!' in usern:
             #send error message to frontend
@@ -31,7 +28,6 @@ class User:
         #
         #
 
-<<<<<<< HEAD
         if db.user_collection.findOne({'username' : usern}).username is None:
             #send error message of username already exists
             return
@@ -39,8 +35,6 @@ class User:
 
 
 
-=======
->>>>>>> 718e7b04766f0b1112aad1688bedc7369680b391
 
         # Create usern, passw, userid, passw_id
         User.username = usern
@@ -50,7 +44,6 @@ class User:
         User.pass_id = encryption.customEncrypt(passw, 4, 1)
 
         #send user object to database as a new user
-<<<<<<< HEAD
         db.user_collection.insertOne(
             {
                 'username': User.username,
@@ -60,22 +53,15 @@ class User:
                 'project_list': User.project_list
             }
         )
-=======
->>>>>>> 718e7b04766f0b1112aad1688bedc7369680b391
 
         return
 
 
-<<<<<<< HEAD
     def login(self,usern, passw, db):
-=======
-    def login(self,usern, passw):
->>>>>>> 718e7b04766f0b1112aad1688bedc7369680b391
         #check database for matching usern and passw
         #if match: get object, put data in object attributes, send object to frontend and return
         #if not match: return error message to frontend
 
-<<<<<<< HEAD
         if db.user_collection.findOne({'username': usern}).username is usern and db.user_collection.findOne({'password' : passw}).password is passw:
             User.username = db.user_collection.findOne({'username': usern}).username
             User.password = db.user_collection.findOne({'username': usern}).password
@@ -90,24 +76,19 @@ class User:
 
     def project_page(self):#implement
         #get all projects of currentUser from database and send to frontend
-        pass
+        cursor = db.user_collection.find({'username': User.username})
+        for temp in cursor:
+            projList = temp['project_list']
+
+        return projList
+
 
     def add_project(self, usern, proj_name, db):
-=======
-        return
-
-    def project_page(self):
-        #get all projects of currentUser from database and send to frontend
-        pass
-
-    def add_project(self, usern, proj_name):
->>>>>>> 718e7b04766f0b1112aad1688bedc7369680b391
         #create new project object
         curProj = Project()
         #make unique_id for that project
         proj_id = encryption.proj_encrypt()
         curProj.add_proj(usern, proj_id, proj_name)
-<<<<<<< HEAD
         curProj.collaborators.append(usern)
 
         #put curProj in database seperately for project object.
@@ -129,28 +110,17 @@ class User:
 
         db.user_collection.updateOne({'username':usern}, {"$set": {'project_list': user_project_list}})
 
-=======
-
-
-        #put curProj in database seperately for project object.
-        # from username, search user object in database, add project to project_list for user
->>>>>>> 718e7b04766f0b1112aad1688bedc7369680b391
 
 
         #send to user object to frontend
         return
 
 
-<<<<<<< HEAD
     def join_project(self, usern, projectid, db):
-=======
-    def join_project(self, usern, projectid):
->>>>>>> 718e7b04766f0b1112aad1688bedc7369680b391
         #check if projectid exist in database
         #if not, return error code to frontend
         #if does, add project to user and add user to project object in both databases
 
-<<<<<<< HEAD
         if db.project_collection.findOne({'project_id': projectid}).project_id is None:
             #return error code
             return
@@ -182,13 +152,5 @@ class User:
 
 
         #send curProj back to frontend
-=======
-        #send user object to frontend
-        return
-
-    def edit_proj(self, usern, projectid):
-        #search proj by proj_id in database
-        #send project object to frontend (including hw1, hw2, collaborators)
->>>>>>> 718e7b04766f0b1112aad1688bedc7369680b391
         return
 
