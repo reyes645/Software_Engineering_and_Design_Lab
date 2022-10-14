102cd2527fe2324564621f09bcb7a8ec2e0e230c
kevinp0122 <kevinpang@ymail.com>
2022-10-14 13:49:57 -0500

    created logintest

--- logintest.py
+++ logintest.py
@@ -1,17 +1,21 @@
 import driver
 from database import Database as db
 from project import Project
-from flask import Flask, request, jsonify
-from flask_jwt_extended import JWTManager, jwt_required, create_access_token
+from flask import Flask, request, jsonify, render_template
+# from flask_jwt_extended import JWTManager, jwt_required, create_access_token
+
+app = Flask(__name__)
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
-        cursor = db.user_collection.find({'username': request.form['username']})  # find the user doc with the token
-        for temp in cursor:
-            actual_password = temp['password']
-        if request.form['password'] == actual_password:
-            return "pass"
+        # cursor = db.user_collection.find({'username': request.form['username']})  # find the user doc with the token
+        # for temp in cursor:
+            # actual_password = temp['password']
+        # if request.form['password'] == actual_password:
+            #return "pass"
         return "fail"
     else:
         return render_template('login.html')
+
+app.run(debug = True, host='0.0.0.0', port=8080)
