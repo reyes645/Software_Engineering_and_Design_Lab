197bcb70f2881da228c9c1ffd3dfc2b4adfb14d6
Lauren Head <laurenhead@utexas.edu>
2022-10-14 13:08:22 -0500

    abhijay's project page frontend code

---
+++ src/index.js
@@ -0,0 +1,123 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import './index.css';
+import {Button, TextField} from '@mui/material';
+
+class HWSET extends React.Component {
+  render() {
+    return <div>HWSET {this.props.properties.name} {this.props.properties.usage}/{this.props.properties.capacity}</div>
+  }
+}
+
+class Project extends React.Component {
+  render() {
+
+    return (
+      <div>
+        <hr />
+        <div>NAME:  {this.props.properties.project_name}</div>
+        <div>USERS: {this.props.properties.users.toString()}</div>
+        <HWSET properties={this.props.properties.hwsets[0]} />
+        <TextField
+          hiddenLabel
+          id="filled-hidden-label-small"
+          variant="filled"
+          size="small"
+        />
+        <Button variant="outlined">Check In</Button>
+        <Button variant="outlined">Check out</Button>
+        <HWSET properties={this.props.properties.hwsets[1]} />
+        <TextField
+          hiddenLabel
+          id="filled-hidden-label-small"
+          variant="filled"
+          size="small"
+        />
+        <Button variant="outlined">Check In</Button>
+        <Button variant="outlined">Check out</Button><br />
+        <Button variant="contained" onClick={()=>this.props.clickJoinLeaveButton()}>{this.props.properties.isJoined ? "LEAVE": "JOIN"}</Button>
+        <hr />
+      </div>
+    );
+  }
+}
+
+class Projects extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      projects: [
+        {
+          project_name: "Project 1",
+          users: ["a", "b"],
+          hwsets: [
+            {
+              name: "1",
+              usage: 50,
+              capacity: 100,
+            },
+            {
+              name: "2",
+              usage: 0,
+              capacity: 100,
+            }
+          ],
+          isJoined: false,
+        },
+        {
+          project_name: "Project 2",
+          users: ["c", "d"],
+          hwsets: [
+            {
+              name: "1",
+              usage: 50,
+              capacity: 100,
+            },
+            {
+              name: "2",
+              usage: 0,
+              capacity: 100,
+            }
+          ],
+          isJoined: true,
+        },
+        {
+          project_name: "Project 3",
+          users: ["e", "f"],
+          hwsets: [
+            {
+              name: "1",
+              usage: 0,
+              capacity: 100,
+            },
+            {
+              name: "2",
+              usage: 0,
+              capacity: 100,
+            }
+          ],
+          isJoined: false,
+        },
+      ]
+    }
+  }
+  render() {
+    const projects = [];
+    for (let i = 0; i < this.state.projects.length; i++) {
+        // note: we are adding a key prop here to allow react to uniquely identify each
+        // element in this array. see: https://reactjs.org/docs/lists-and-keys.html
+        projects.push(<Project key={i} properties={this.state.projects[i]} clickJoinLeaveButton={()=>{var cpy = JSON.parse(JSON.stringify(this.state.projects)); cpy[i].isJoined = !cpy[i].isJoined; console.log(cpy); this.setState({projects: cpy})}} />);
+    }
+    return (
+      <div>
+        <p>Projects</p>
+        <div>{projects}</div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Projects />);
